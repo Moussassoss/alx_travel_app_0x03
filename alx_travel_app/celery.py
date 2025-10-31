d40910f01ef30ad619f080d4f1d605e89b7ef807
Moussa Mahamat Nassour <132145833+Moussassoss@users.noreply.github.com>
2025-10-31 16:24:58 +0200

    Create celery.py

---
+++ alx_travel_app/celery.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, unicode_literals
+import os
+from celery import Celery
+
+# set default Django settings module
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alx_travel_app.settings')
+
+app = Celery('alx_travel_app')
+
+# Load task modules from all registered Django app configs
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+
+@app.task(bind=True)
+def debug_task(self):
+    print(f'Request: {self.request!r}')
